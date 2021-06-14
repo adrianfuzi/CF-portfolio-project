2e434351d8d9a56a856b373f27c685be2ce3d66c
adrianfuzi <37154180+adewfalcon@users.noreply.github.com>
2021-06-14 16:15:07 +0200

    Add animated SVG, fix hero text alignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Future scripts here
+// Set SVG image width
+const imgWidth = document.getElementById("img").offsetWidth;
+const svg = document.getElementById("undraw_town_r6pc");
+
+function setWidth() {
+  svg.style.width = imgWidth + "px";
+}
+
+setWidth();
